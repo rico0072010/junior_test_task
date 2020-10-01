bde3b206841f17e58ff8a24ef26cd2b27a828ddc
Danil Dolin <danildolin96@gmail.com>
2020-10-01 14:10:02 +0500

    Create user and setup localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: :home
+
+  def set_locale
+    if user_signed_in?
+      current_user.language
+    else
+      I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
+    end
+  end
+
+  def locale_from_header
+    request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
+  end
+
   def home
     render html: 'Hello World!'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username language])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username language])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,12 @@ module JuniorTestTask
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.available_locales = %i[en ru]
+    config.i18n.default_locale = :ru
+    config.i18n.fallbacks = true
   end
 end
